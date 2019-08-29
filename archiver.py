22030452192da1561b6402776424457ce3582b42
iElden <Elden@vps586160.localdomain>
2019-08-30 01:29:50 +0200

    archiver now give display_name insead of discord name

--- archiver.py
+++ archiver.py
@@ -25,7 +25,7 @@ async def archive(message, channel):
     await ar.send(f"**==== #{channel.name} ====**\n\n{channel.topic if channel.topic else ''}")
     async for msg in channel.history(limit=None, before=message.created_at, oldest_first=True):
         em = discord.Embed(description=msg.content, timestamp=msg.created_at, colour=msg.author.colour)
-        em.set_author(name=msg.author.name, icon_url=msg.author.avatar_url)
+        em.set_author(name=msg.author.display_name, icon_url=msg.author.avatar_url)
         em.set_footer(text=channel.name)
         await ar.send(embed=em)
     await channel.send("Terminé (par sécurité, la supression du canal n'est pas activé, elle le sera quand tout sera au point)")
